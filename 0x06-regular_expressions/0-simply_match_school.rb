98bdb8992a54c0beae80b114414c15e4bed46ecd
Kamalkiro <125470846+Kamalkiro@users.noreply.github.com>
2024-05-06 17:54:22 +0100

    Delete 0x06-regular_expressions directory

--- 0x06-regular_expressions/0-simply_match_school.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-# This regex expression accepts one argument and pass it to a regular expression matching method
-
-puts ARGV[0].scan(/School/).join
--- 0x06-regular_expressions/100-textme.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-# This script outputs: [SENDER],[RECEIVER],[FLAGS]
-
-puts ARGV[0].scan(/\[from:(.*)\] \[to:(.*)\] \[flags:(.*?)\]/).join(",")
